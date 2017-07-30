6079b168dbb401c6ca1106a9d4463ac2840ca213
Michael98Liu <liufengyuan1998@hotmail.com>
2017-07-30 16:45:06 -0700

    Bug fixed

--- index_builder/reader.hpp
+++ index_builder/reader.hpp
@@ -8,13 +8,13 @@
 class Reader{
 public:
 
-	std::vector<char> read_com(ifstream& infile);
+	std::vector<char> read_com(std::ifstream& infile);
 
-	std::vector<unsigned int> VBDecode(ifstream& ifile, long start_pos = 0, long end_pos = ifile.tellg());
+	std::vector<unsigned int> VBDecode(std::ifstream& ifile, long start_pos = 0, long end_pos = ifile.tellg());
 
 	std::vector<unsigned int> VBDecode(std::vector<char>& vec);
 
-	std::vector<Posting> decompress(std::string filename, unsigned int termID, map<unsigned int, mData>& dict);
+	std::vector<Posting> decompress(std::string filename, unsigned int termID, std::map<unsigned int, mData>& dict);
 };
 
 #endif
