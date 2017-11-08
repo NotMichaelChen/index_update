4afd8175622f1d9c67232642dbd6e1800b1aa4dd
Michael98Liu <liufengyuan1998@hotmail.com>
2017-11-08 12:47:38 +0400

    Compress method issue

--- index/extended_lexicon.cpp
+++ index/extended_lexicon.cpp
@@ -1,13 +1,4 @@
 #include "extended_lexicon.hpp"
-
-<<<<<<< HEAD
-void ExtendedLexicon::addNonPositional( std::string term, mData entry ){
-        exlexnp[term].push_back(entry);
-}
-
-void ExtendedLexicon::addPositional( std::string term, mData entry ){
-    exlexp[term].push_back(entry);
-=======
 #include <unordered_map>
 #include <vector>
 
