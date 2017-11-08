e1bf2a4af59bd032b60f645a07dabce190fba3d9
NotMichaelChen <mzc12345@gmail.com>
2017-11-07 20:25:09 -0500

    Fixed function header mismatch in doc analyzer

--- index/doc_analyzer/analyzer.h
+++ index/doc_analyzer/analyzer.h
@@ -14,17 +14,17 @@
 struct MatcherInfo {
     MatcherInfo(std::vector<ExternNPposting> n, std::vector<ExternPposting> p, std::vector<Matcher::Translation> t, Matcher::StringEncoder s, int m)
         : NPpostings(n), Ppostings(p), translations(t), se(s), maxfragID(m) {}
-    
+
     std::vector<ExternNPposting> NPpostings;
     std::vector<ExternPposting> Ppostings;
     std::vector<Matcher::Translation> translations;
     Matcher::StringEncoder se;
-    
+
     int maxfragID;
 };
 
 //Updates the index given a new page
-MatcherInfo indexUpdate(std::string& url, std::string& newpage, Structures::DocumentStore& docstore, Structures::TranslationTable& transtable);
+MatcherInfo indexUpdate(std::string& url, std::string& newpage, std::string& timestamp, Structures::DocumentStore& docstore, Structures::TranslationTable& transtable);
 //Generates new postings and translations from the new page
 MatcherInfo makePosts(Structures::DocumentTuple& olddoc, std::string& newpage);
 
