5fb8509d3fd02b1e4c21bfda0dc48e5438ba418f
Michael98Liu <liufengyuan1998@hotmail.com>
2017-11-24 19:19:39 +0400

    Modified Read Directory Function

--- index/main.cpp
+++ index/main.cpp
@@ -10,14 +10,21 @@ using namespace std;
 
 //http://forum.codecall.net/topic/60157-read-all-files-in-a-folder/
 vector<string> openInDir(string path = ".") {
+    cout << "reading files." << endl;
     DIR*    dir;
     dirent* pdir;
     vector<string> files;
 
-    dir = opendir(path.c_str());
+    dir = opendir( path.empty() ? "." : path.c_str() );
 
-    while (pdir = readdir(dir)) {
-        files.push_back(pdir->d_name);
+    if( dir ){
+        while (true){
+  			pdir = readdir( dir );
+  			if (pdir == NULL) break;
+            string d_n(pdir->d_name);
+  			files.push_back( d_n );
+      	}
+    	closedir( dir );
     }
 
     return files;
@@ -34,7 +41,10 @@ int main(int argc, char **argv) {
     Index index;
     vector<string> filelist = openInDir("./dataset-format");
 
+    cout << "Files read finished." << endl;
+
     for(string& i : filelist) {
+
         if(i == "." || i == "..")
             continue;
         string filename;
@@ -49,7 +59,6 @@ int main(int argc, char **argv) {
         ifstream inputfile("./dataset-format/" + i);
         //https://www.reddit.com/r/learnprogramming/comments/3qotqr/how_can_i_read_an_entire_text_file_into_a_string/cwh8m4d/
         string filecontents{ istreambuf_iterator<char>(inputfile), istreambuf_iterator<char>() };
-
 
         cout << "Inserting " << filename << endl;
         index.insert_document(filename, filecontents);
