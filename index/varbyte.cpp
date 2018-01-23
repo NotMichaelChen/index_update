9b9e9820e08b4e18a9df335bc412c949f9dc7f60
NotMichaelChen <mzc12345@gmail.com>
2018-01-23 02:31:16 -0500

    Rewrote varbyte functions

--- index/varbyte.cpp
+++ index/varbyte.cpp
@@ -2,119 +2,53 @@
 
 #include <bitset>
 #include <math.h>
+#include <algorithm>
 
-std::vector<uint8_t> VBEncode(unsigned int num){
-	/**
-	 * Variable byte encoding.
-	 *
-	 * @param: an umsigned interger to be compressed
-	 * @return: a vector of bytes
-	 */
-	std::vector<uint8_t> result;
-	uint8_t b;
-	while(num >= 128){
-		int a = num % 128;
-		std::bitset<8> byte(a);
-		byte.flip(7);
-		num = (num - a) / 128;
-		b = byte.to_ulong();
-		result.push_back(b);
-	}
-	int a = num % 128;
-	std::bitset<8> byte(a);
-	b = byte.to_ulong();
-	result.push_back(b);
-	return result;
+//https://nlp.stanford.edu/IR-book/html/htmledition/variable-byte-codes-1.html
+
+std::vector<uint8_t> VBEncode(unsigned int num) {
+    std::vector<uint8_t> bytes;
+
+    while(true) {
+        bytes.push_back(num % 128);
+        if(num < 128)
+            break;
+        num /= 128;
+    }
+    std::reverse(bytes.begin(), bytes.end());
+    bytes[bytes.size() - 1] += 128;
+    return bytes;
 }
 
-std::vector<uint8_t> VBEncode(std::vector<unsigned int>& nums){
-	/**
-	 * Encode a std::vector of unsigned intergers.
-	 */
-	std::vector<uint8_t> biv;
-	std::vector<uint8_t> result;
-	for( std::vector<unsigned int>::iterator it = nums.begin(); it != nums.end(); it ++){
-		biv = VBEncode(*it);
-		result.insert(result.end(), biv.begin(), biv.end());
-	}
-	return result;
+std::vector<uint8_t> VBEncode(std::vector<unsigned int>& nums) {
+    std::vector<uint8_t> bytestream;
+
+    for(const unsigned int &n : nums) {
+        std::vector<uint8_t> bytes = VBEncode(n);
+        bytestream.insert(bytestream.end(), bytes.begin(), bytes.end());
+    }
+
+    return bytestream;
 }
 
-//compr_ind is the vector returned from read_com
-std::vector<unsigned int> VBDecode(std::ifstream& ifile, std::vector<char>& compr_ind, long start_pos, long end_pos){//ios::ate
-	ifile.seekg(start_pos);
-	char c;
-	unsigned int num;
-	int p;
-	std::vector<unsigned int> result;
-    if(end_pos == 0) end_pos = ifile.end;
+std::vector<unsigned int> VBDecode(std::vector<uint8_t> bytestream) {
+    std::vector<unsigned int> numbers;
+    int n = 0;
 
-	for(std::vector<char>::iterator it = compr_ind.begin(); it != compr_ind.end(); it++){
-		c = *it;
-		std::bitset<8> byte(c);
-		num = 0;
-		p = 0;
-		while(byte[7] == 1){
-			byte.flip(7);
-			num += byte.to_ulong()*pow(128, p);
-			p++;
-			it ++;
-			c = *it;
-			byte = std::bitset<8>(c);
-		}
-		num += (byte.to_ulong())*pow(128, p);
-
-		result.push_back(num);
-	}
-	return result;
+    for(size_t i = 0; i < bytestream.size(); i++) {
+        if(bytestream[i] < 128) {
+            n = 128 * n + bytestream[i];
+        }
+        else {
+            n = 128 * n + (bytestream[i] - 128);
+            numbers.push_back(n);
+            n = 0;
+        }
+    }
+    return numbers;
 }
 
-std::vector<unsigned int> VBDecode(std::vector<char>& vec){
-	unsigned int num;
-	std::vector<unsigned int> result;
-	char c;
-	int p;
-	for(std::vector<char>::iterator it = vec.begin(); it != vec.end(); it++){
-		c = *it;
-		std::bitset<8> byte(c);
-		num = 0;
-		p = 0;
-		while(byte[7] == 1){
-			byte.flip(7);
-			num += byte.to_ulong()*pow(128, p);
-			p++;
-			it ++;
-			c = *it;
-			byte = std::bitset<8>(c);
-		}
-		num += (byte.to_ulong())*pow(128, p);
-
-		result.push_back(num);
-	}
-	return result;
-}
-
-std::vector<unsigned int> VBDecode(char* buffer, int length){
-	unsigned int num;
-	std::vector<unsigned int> result;
-	char c;
-	int p;
-	for(int i = 0; i < length; i ++ ){
-		c = buffer[i];
-		std::bitset<8> byte(c);
-		num = 0;
-		p = 0;
-		while(byte[7] == 1){
-			byte.flip(7);
-			num += byte.to_ulong()*pow(128, p);
-			p++;
-			i ++;
-			c = buffer[i];
-			byte = std::bitset<8>(c);
-		}
-		num += (byte.to_ulong())*pow(128, p);
-
-		result.push_back(num);
-	}
-	return result;
+std::vector<unsigned int> VBDecode(char* buffer, int length) {
+    std::vector<uint8_t> bytestream(buffer, buffer + length);
+    return VBDecode(bytestream);
 }
--- index/varbyte.hpp
+++ index/varbyte.hpp
@@ -6,8 +6,7 @@
 
 std::vector<uint8_t> VBEncode(unsigned int num);
 std::vector<uint8_t> VBEncode(std::vector<unsigned int>& nums);
-std::vector<unsigned int> VBDecode(std::ifstream& ifile, long start_pos = 0, long end_pos = 0);
-std::vector<unsigned int> VBDecode(std::vector<char>& vec);
+std::vector<unsigned int> VBDecode(std::vector<uint8_t> bytestream);
 std::vector<unsigned int> VBDecode(char* buffer, int length);
 
 #endif
