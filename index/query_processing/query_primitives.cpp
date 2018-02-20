e6b4cf6c975b5d709f0daff1e668b22bd78e0644
NotMichaelChen <mzc12345@gmail.com>
2018-02-19 19:28:22 -0500

    Began implementing nextGEQ

--- index/query_processing/query_primitives.cpp
+++ index/query_processing/query_primitives.cpp
@@ -6,6 +6,7 @@
 query_primitive::query_primitive(int termID, std::map<unsigned int, std::vector<nPosting>>& index) {
     inmemory = true;
     postinglist = index[termID];
+    postingindex = 0;
 }
 
 query_primitive::query_primitive(int termID, std::vector<mData>::iterator mdata) {
@@ -24,12 +25,47 @@ query_primitive::query_primitive(int termID, std::vector<mData>::iterator mdata)
     buffersize = metadata->postings_blocks - metadata->blocksizes;
     blocksizes = read_block(buffersize, ifile, VBDecode, false);
 
-    //Decompress and store the first docID and frequency block
+    //Decompress and store the first docID
+    //Only decompress frequency block if getFreq is called
     buffersize = blocksizes[0];
     docblock = read_block(buffersize, ifile, VBDecode, true);
 
-    buffersize = blocksizes[1];
-    freqblock = read_block(buffersize, ifile, VBDecode, false);
+    docIDindex = 1;
+}
 
-    blockindex = 2;
+unsigned int query_primitive::nextGEQ(unsigned int pos) {
+    if(inmemory) {
+        while(postingindex < postinglist.size() && postinglist[postingindex].docID < pos) {
+            ++postingindex;
+        }
+        //Never go out of bounds
+        //TODO: determine what to return if out of bounds
+        if(postingindex == postinglist.size())
+            --postingindex;
+        return postinglist[postingindex].docID;
+    }
+    else {
+        size_t oldindex = docIDindex;
+        //Find the correct block to look at
+        while(docIDindex < last_docID.size() && last_docID[docIDindex] < pos) {
+            //Seek to next block location
+            long amount = blocksizes[(docIDindex*2)-1] + blocksizes[(docIDindex*2)];
+            ifile.seekg(amount, std::ios_base::cur);
+
+            ++docIDindex;
+        }
+        //If it's different than our current block then decompress new block
+        if(oldindex != docIDindex) {
+            size_t buffersize = blocksizes[docIDindex*2];
+            docblock = read_block(buffersize, ifile, VBDecode, true);
+
+            blockindex = 0;
+        }
+
+        //Perform standard docID searching
+        while(blockindex < docblock.size() && docblock[blockindex] < pos)
+            ++blockindex;
+        
+        return docblock[blockindex];
+    }
 }
--- index/query_processing/query_primitives.hpp
+++ index/query_processing/query_primitives.hpp
@@ -13,15 +13,25 @@ public:
     query_primitive(int termID, std::map<unsigned int, std::vector<nPosting>>& index);
     query_primitive(int termID, std::vector<mData>::iterator mdata);
 
+    //advances the read pointer of lp to the posting with the smallest
+    //docID that is at least x, and then returns that docID. Note that read pointers only move
+    //forward; thus, if the pointer currently points to a posting with docID y > x, then the pointer
+    //will not be moved, and y will be returned
+    unsigned int nextGEQ(unsigned int pos);
+
 private:
     bool inmemory;
     
     std::vector<nPosting> postinglist;
+    size_t postingindex;
 
     std::vector<mData>::iterator metadata;
     std::vector<unsigned int> last_docID;
     std::vector<unsigned int> blocksizes;
 
+    //Which docID block to examine next
+    size_t docIDindex;
+    //Which number in the block we are currently pointed to
     size_t blockindex;
 
     std::vector<unsigned int> docblock;
