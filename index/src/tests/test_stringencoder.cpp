baa0390052250c27961c6543a0dfda816fddbb90
NotMichaelChen <mzc12345@gmail.com>
2018-06-14 15:51:08 -0400

    Fixed test case

--- index/src/tests/test_stringencoder.cpp
+++ index/src/tests/test_stringencoder.cpp
@@ -3,35 +3,35 @@
 #include "doc_analyzer/Matcher/stringencoder.h"
 
 TEST_CASE("Test stringencoder", "[stringencoder]") {
-    Matcher::StringEncoder se("a b c", "c b a");
+    Matcher::StringEncoder se("a b c d", "c b a e");
 
-    std::vector<unsigned int> oldstream(se.getOldIter(), se.getOldEnd());
-    std::vector<unsigned int> newstream(se.getNewIter(), se.getNewEnd());
+        std::vector<unsigned int> oldstream(se.getOldIter(), se.getOldEnd());
+        std::vector<unsigned int> newstream(se.getNewIter(), se.getNewEnd());
 
-    std::vector<std::string> oldwords = se.decodeStream(oldstream);
-    std::vector<std::string> newwords = se.decodeStream(newstream);
+        std::vector<std::string> oldwords = se.decodeStream(oldstream);
+        std::vector<std::string> newwords = se.decodeStream(newstream);
 
-    REQUIRE(oldwords.size() == 3);
-    REQUIRE(newwords.size() == 3);
+        REQUIRE(oldwords.size() == 4);
+        REQUIRE(newwords.size() == 4);
 
-    REQUIRE(oldwords[0] == "a");
-    REQUIRE(oldwords[1] == "b");
-    REQUIRE(oldwords[2] == "c");
+        REQUIRE(oldwords[0] == "a");
+        REQUIRE(oldwords[1] == "b");
+        REQUIRE(oldwords[2] == "c");
+        REQUIRE(oldwords[3] == "d");
 
-    REQUIRE(newwords[0] == "c");
-    REQUIRE(newwords[1] == "b");
-    REQUIRE(newwords[2] == "a");
-    // REQUIRE(se.decodeStream(oldstream) == {"a", "b", "c"});
-    // REQUIRE(se.decodeStream(newstream) == "c b a");
+        REQUIRE(newwords[0] == "c");
+        REQUIRE(newwords[1] == "b");
+        REQUIRE(newwords[2] == "a");
+        REQUIRE(newwords[3] == "e");
 
     std::string s = "a";
     REQUIRE(se.getNewCount(s) == 1);
 
-    s = "b";
+    s = "e";
     REQUIRE(se.inNew(s));
     s = "d";
     REQUIRE(!se.inNew(s));
-    s = "a";
+    s = "d";
     REQUIRE(se.inOld(s));
     s = "sdifjoaeariog reg r ";
     REQUIRE(!se.inOld(s));
