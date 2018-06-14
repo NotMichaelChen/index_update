2116435f451bc79283dcb097c7687b888d7fbc7e
NotMichaelChen <mzc12345@gmail.com>
2018-06-14 15:45:29 -0400

    Added stringencoder tests

---
+++ index/src/tests/test_stringencoder.cpp
@@ -0,0 +1,38 @@
+#include "libs/catch.hpp"
+
+#include "doc_analyzer/Matcher/stringencoder.h"
+
+TEST_CASE("Test stringencoder", "[stringencoder]") {
+    Matcher::StringEncoder se("a b c", "c b a");
+
+    std::vector<unsigned int> oldstream(se.getOldIter(), se.getOldEnd());
+    std::vector<unsigned int> newstream(se.getNewIter(), se.getNewEnd());
+
+    std::vector<std::string> oldwords = se.decodeStream(oldstream);
+    std::vector<std::string> newwords = se.decodeStream(newstream);
+
+    REQUIRE(oldwords.size() == 3);
+    REQUIRE(newwords.size() == 3);
+
+    REQUIRE(oldwords[0] == "a");
+    REQUIRE(oldwords[1] == "b");
+    REQUIRE(oldwords[2] == "c");
+
+    REQUIRE(newwords[0] == "c");
+    REQUIRE(newwords[1] == "b");
+    REQUIRE(newwords[2] == "a");
+    // REQUIRE(se.decodeStream(oldstream) == {"a", "b", "c"});
+    // REQUIRE(se.decodeStream(newstream) == "c b a");
+
+    std::string s = "a";
+    REQUIRE(se.getNewCount(s) == 1);
+
+    s = "b";
+    REQUIRE(se.inNew(s));
+    s = "d";
+    REQUIRE(!se.inNew(s));
+    s = "a";
+    REQUIRE(se.inOld(s));
+    s = "sdifjoaeariog reg r ";
+    REQUIRE(!se.inOld(s));
+}
