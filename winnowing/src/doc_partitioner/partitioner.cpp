c7c7183fa5c0595b6b95da64ed12cb4d5d246f63
hieusydo <hieusydo@gmail.com>
2018-12-23 15:08:05 -0500

    Fixed winnowing bug

--- winnowing/src/doc_partitioner/partitioner.cpp
+++ winnowing/src/doc_partitioner/partitioner.cpp
@@ -9,17 +9,26 @@ Partitioner::Partitioner(const unsigned int b, const unsigned int w) : B_WINNOW(
 
 std::vector<Fragment> Partitioner::partitionPage(size_t did, const std::string& origPage) {
     std::vector<Fragment> partitionres;
+
     std::vector<std::string> normalized = normalizeFile(origPage);
+    std::cout << "Normalizing result size: " << normalized.size() << '\n';
+
     std::vector<unsigned char> wordHashed = wordHashFile(normalized);
+    std::cout << "Word hashing result size: " << wordHashed.size() << '\n';
+
     std::vector<size_t> hashed = hashFile(wordHashed);
+    std::cout << "Substring hashing result size: " << hashed.size() << '\n';
+
     std::vector<size_t> cut = cutFile(hashed);
+    std::cout << "Cut result size: " << cut.size() << '\n';
+
     size_t lastCut = 0;
-    size_t cutSz = 0;
+    size_t totalcutsize = 0;
     size_t cutNo = 0;
     for (size_t i = 0; i < cut.size(); ++i) {
         if (cut[i]) {
-            std::cout << "Cut #" << (++cutNo) << " - size: " << (i - lastCut) << '\n';
-            cutSz += (i - lastCut);
+            std::cout << "Cut #" << (++cutNo) << " - size: " << (i - lastCut + 1) << '\n';
+            totalcutsize += (i - lastCut + 1);
             Fragment newfrag;
             newfrag.docid = did;
             newfrag.startpos = lastCut;
@@ -35,6 +44,7 @@ std::vector<Fragment> Partitioner::partitionPage(size_t did, const std::string& 
 }
 
 std::vector<std::string> Partitioner::normalizeFile(const std::string& origFile) {
+    std::cout << "Normalizing page...\n";
     std::vector<std::string> processResult;
     std::vector<std::string> words = StringUtil::splitString(origFile, " \n\t\r\f!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~");
     for(const std::string& w : words) {
@@ -45,6 +55,7 @@ std::vector<std::string> Partitioner::normalizeFile(const std::string& origFile)
 }
 
 std::vector<unsigned char> Partitioner::wordHashFile(const std::vector<std::string>& normalized) {
+    std::cout << "Hashing each word in page...\n";
     std::vector<unsigned char> wordHashRes;
     std::hash<std::string> strHash;
     for(const std::string& w : normalized) {
@@ -54,8 +65,17 @@ std::vector<unsigned char> Partitioner::wordHashFile(const std::vector<std::stri
 }
 
 std::vector<size_t> Partitioner::hashFile(const std::vector<unsigned char>& file) {
-    std::vector<size_t> hashResult(file.size() - B_WINNOW);
+    std::cout << "Hashing substrings in page...\n";
     std::hash<std::string> strHash;
+
+    if (file.size() <= B_WINNOW) {
+        std::string s_i = std::string(file.begin(), file.end());
+        std::vector<size_t> hashResult;
+        hashResult.push_back(strHash(s_i));
+        return hashResult;
+    }
+
+    std::vector<size_t> hashResult(file.size() - B_WINNOW);
     for (size_t i = 0; i < hashResult.size(); ++i) {
         std::string s_i = std::string(file.begin()+i, file.begin()+i+B_WINNOW-1);
         hashResult[i] = strHash(s_i);
@@ -64,6 +84,7 @@ std::vector<size_t> Partitioner::hashFile(const std::vector<unsigned char>& file
 }
 
 std::vector<size_t> Partitioner::cutFile(const std::vector<size_t>& hashedFile) {
+    std::cout << "Cutting page...\n";
     std::vector<size_t> cutResults(hashedFile.size(), 0);
 
     if (cutResults.size() <= W_WINNOW) {
